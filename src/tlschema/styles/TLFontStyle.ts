3cb896128ecd50ab910a0530a4892a1c91ad2078
OhMinSeop <mins5190@gmail.com>
2023-06-09 21:13:20 +0900

    tlschema 스타일 생성

---
+++ src/tlschema/styles/TLFontStyle.ts
@@ -0,0 +1,23 @@
+import { T } from "../../tlvalidate";
+import { SetValue } from "../util-types";
+import { TLBaseStyle } from "./TLBaseStyle";
+
+/** @public */
+export const TL_FONT_TYPES = new Set([
+  "draw",
+  "sans",
+  "serif",
+  "mono",
+] as const);
+
+/** @public */
+export type TLFontType = SetValue<typeof TL_FONT_TYPES>;
+
+/** @public */
+export interface TLFontStyle extends TLBaseStyle {
+  id: TLFontType;
+  type: "font";
+}
+
+/** @internal */
+export const fontValidator = T.setEnum(TL_FONT_TYPES);
---
+++ src/tlschema/styles/style-types.ts
@@ -0,0 +1,44 @@
+import { TLAlignStyle } from "./TLAlignStyle";
+import { TLArrowheadEndStyle, TLArrowheadStartStyle } from "./TLArrowheadStyle";
+import { TLStyleType } from "./TLBaseStyle";
+import { TLColorStyle } from "./TLColorStyle";
+import { TLDashStyle } from "./TLDashStyle";
+import { TLFillStyle } from "./TLFillStyle";
+import { TLFontStyle } from "./TLFontStyle";
+import { TLGeoStyle } from "./TLGeoStyle";
+import { TLSizeStyle } from "./TLSizeStyle";
+import { TLSplineStyle } from "./TLSplineStyle";
+import { TLVerticalAlignStyle } from "./TLVerticalAlignStyle";
+
+/** @public */
+export type TLStyleItem =
+  | TLColorStyle
+  | TLFillStyle
+  | TLDashStyle
+  | TLSizeStyle
+  | TLFontStyle
+  | TLAlignStyle
+  | TLVerticalAlignStyle
+  | TLGeoStyle
+  | TLArrowheadStartStyle
+  | TLArrowheadEndStyle
+  | TLSplineStyle;
+//	| TLIconStyle
+
+/** @public */
+export interface TLStyleCollections {
+  color: TLColorStyle[];
+  fill: TLFillStyle[];
+  dash: TLDashStyle[];
+  size: TLSizeStyle[];
+  font: TLFontStyle[];
+  align: TLAlignStyle[];
+  verticalAlign: TLVerticalAlignStyle[];
+  geo: TLGeoStyle[];
+  arrowheadStart: TLArrowheadStartStyle[];
+  arrowheadEnd: TLArrowheadEndStyle[];
+  spline: TLSplineStyle[];
+}
+
+/** @public */
+export type TLStyleProps = Pick<TLShapeProps, TLStyleType>;
